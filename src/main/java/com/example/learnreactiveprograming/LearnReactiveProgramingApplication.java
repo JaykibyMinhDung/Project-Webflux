dbf58154ce8a05e33b38d6c163c365d9aad9e24c
MinhDungJKB <doanminhdung1729@gmail.com>
2025-02-09 21:35:37 +0700

    beginer webflux

---
+++ src/main/java/com/example/learnreactiveprograming/LearnReactiveProgramingApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnreactiveprograming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactiveProgramingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnReactiveProgramingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnreactiveprograming/LearnReactiveProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnreactiveprograming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactiveProgramingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
